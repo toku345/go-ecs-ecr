04a11ee177a57bd77c12ef6609b1187d7d08a1fc
Fumitaka Tokumitsu <toku345@gmail.com>
2017-05-11 16:05:16 +0900

    Change region2

--- deploy.sh
+++ deploy.sh
@@ -9,32 +9,32 @@ AWS_ECR_REP_NAME=nginx-sample-webapp
 
 # Create Task Definition
 make_task_def(){
-	task_template='[
-		{
-			"name": "%s",
-			"image": "%s.dkr.ecr.%s.amazonaws.com/%s:%s",
-			"essential": true,
-			"memory": 200,
-			"cpu": 10,
-			"portMappings": [
-				{
-					"containerPort": 80,
-					"hostPort": 80
-				}
-			]
-		}
-	]'
+    task_template='[
+    {
+      "name": "%s",
+      "image": "%s.dkr.ecr.%s.amazonaws.com/%s:%s",
+      "essential": true,
+      "memory": 200,
+      "cpu": 10,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ]
+    }
+  ]'
 
-	task_def=$(printf "$task_template" ${AWS_ECS_TASKDEF_NAME} $AWS_ACCOUNT_ID ${AWS_DEFAULT_REGION} ${AWS_ECR_REP_NAME} $CIRCLE_SHA1)
+    task_def=$(printf "$task_template" ${AWS_ECS_TASKDEF_NAME} $AWS_ACCOUNT_ID ${AWS_DEFAULT_REGION} ${AWS_ECR_REP_NAME} $CIRCLE_SHA1)
 }
 
 # more bash-friendly output for jq
 JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
-	aws --version
-	aws configure set default.region ${AWS_DEFAULT_REGION}
-	aws configure set default.output json
+    aws --version
+    aws configure set default.region ${AWS_DEFAULT_REGION}
+    aws configure set default.output json
 }
 
 deploy_cluster() {
@@ -42,7 +42,7 @@ deploy_cluster() {
     make_task_def
     register_definition
     if [[ $(aws ecs update-service --cluster ${AWS_ECS_CLUSTER_NAME} --service ${AWS_ECS_SERVICE_NAME} --task-definition $revision | \
-                   $JQ '.service.taskDefinition') != $revision ]]; then
+                $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
     fi
@@ -66,8 +66,8 @@ deploy_cluster() {
 
 
 push_ecr_image(){
-	eval $(aws ecr get-login --region ${AWS_DEFAULT_REGION})
-	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/${AWS_ECR_REP_NAME}:$CIRCLE_SHA1
+    eval $(aws ecr get-login --region ${AWS_DEFAULT_REGION})
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/${AWS_ECR_REP_NAME}:$CIRCLE_SHA1
 }
 
 register_definition() {
